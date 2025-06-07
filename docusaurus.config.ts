d5a56fb1f2ad8df2eae11e94ee5da14c2fe86aec
Loning <aloning@gmail.com>
2025-06-07 17:42:22 +0800

    private

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   url: 'https://loning.github.io', // Replace 'auric' with actual GitHub username
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/alltheory/',
+  baseUrl: '/private-loning-auric-alltheory/',
 
   // GitHub pages deployment config.
   organizationName: 'loning', // Replace with actual GitHub username
